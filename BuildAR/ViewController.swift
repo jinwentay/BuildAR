f45dd5ad4cd798295399eb384adaa4c96c0a275b
Tay Jin Wen <e0325124@u.nus.edu>
2019-06-06 00:29:36 +0800

    entry point page created for app

--- BuildAR/ViewController.swift
+++ BuildAR/ViewController.swift
@@ -13,21 +13,22 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    @IBOutlet weak var createWorldBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Set the view's delegate
-        sceneView.delegate = self
+       /* sceneView.delegate = self
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
-        sceneView.scene = scene
+        sceneView.scene = scene*/
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -37,14 +38,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let configuration = ARWorldTrackingConfiguration()
 
         // Run the view's session
-        sceneView.session.run(configuration)
+        //sceneView.session.run(configuration)
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
         // Pause the view's session
-        sceneView.session.pause()
+        //sceneView.session.pause()
     }
 
     // MARK: - ARSCNViewDelegate
